610d1b18c813e818697c610e152dd92b448a736a
tostitoalfa <andrespdiaz1978@gmail.com>
2025-04-10 19:06:24 -0700

    cambiada la llave de la api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const searchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id='
+    const url = 'https://api.unsplash.com/search/photos/?client_id=_r-2p6HBqR4RxcQD5ohhgUC4_24V2ualU2OEX2lgWAg'
     const res = await axios.get(url,{
         params:{
             query: term
